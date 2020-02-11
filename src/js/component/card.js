113f16abd11a8f1399c2ea88b7a753e9474a707b
Brenda Matos <brenda.matosux@gmail.com>
2020-02-11 01:13:44 +0000

    functionality added

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,20 +1,32 @@
-// import React from "react";
-// import PropTypes from "prop-types";
+import React, { useState } from "react";
 
-// export function Card(props) {
-// 	return (
-// 		<div className="card d-flex mx-auto" style={{ width: "32rem" }}>
-// 			<div className="card-body">
-// 				<ul className="list-group list-group-flush">
-// 					<li className="list-group-item">{props.cardTodo}</li>
-// 					<li className="list-group-item" />
-// 					<li className="list-group-item" />
-// 				</ul>
-// 			</div>
-// 		</div>
-// 	);
-// }
+export function Card() {
+	const [list, setList] = useState([]);
+	const [content, setContent] = useState();
 
-// Card.propTypes = {
-// 	cardTodo: PropTypes.string
-// };
+	return (
+		<div className="card d-flex mx-auto" style={{ width: "32rem" }}>
+			<div className="card-body">
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item">
+						<input
+							value={content}
+							onChange={e => setContent(e.target.value)}
+							onKeyPress={e => {
+								if (e.key === "Enter") {
+									setList(list.concat(content));
+									setContent("Add your tasks");
+								}
+							}}
+						/>
+						{list.map((item, index) => {
+							return <div key={index}>{item}</div>;
+						})}
+					</li>
+					<li className="list-group-item" />
+					<li className="list-group-item" />
+				</ul>
+			</div>
+		</div>
+	);
+}
