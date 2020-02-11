a67cee0a1b12b769dda5fecf992430d645efbc94
Brenda Matos <brenda.matosux@gmail.com>
2020-02-11 00:16:13 +0000

    css updates

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-<div classNameName="card" style="width: 18rem;">
-	<ul className="list-group list-group-flush">
-		<li className="list-group-item">Cras justo odio</li>
-		<li className="list-group-item">Dapibus ac facilisis in</li>
-		<li className="list-group-item">Vestibulum at eros</li>
-	</ul>
-</div>;
+export function Card() {
+	return (
+		<div className="card d-flex mx-auto" style={{ width: "18rem" }}>
+			<div className="card-body">
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item">Cras justo odio</li>
+					<li className="list-group-item">Dapibus ac facilisis in</li>
+					<li className="list-group-item">Vestibulum at eros</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
